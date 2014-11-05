7305bcd5d4a3b380fb12c3a3ed415bf775729875
prasanna0586 <prasannakumar.ramachandran@object-frontier.com>
2014-11-05 14:58:19 +0530

    Final Checkin

--- scripts/controller.js
+++ scripts/controller.js
@@ -121,32 +121,85 @@
 	{
 		$scope.saveUserDetails = function()
 		{
-			if(validateName() && validatePhoneNumber())
+			if(validateName() && validateAge() && validatePhoneNumber() && validateEmail())
 			{
 				alert("User Details saved successfully");
+				$scope.newUser.name = "";
+				$scope.newUser.phone = "";
+				$scope.newUser.age = "";
+				$scope.newUser.address = "";
+				$scope.newUser.email = "";
+				document.getElementById('output').src = '';
 			}
-		};	
-		function validatePhoneNumber()
+		};
+		
+		$scope.openFile = function()
 		{
-			var isValidPhoneNumber = true;
-			if($scope.newUser.phone.length > 10)
-			{
-				isValidPhoneNumber = false;
-				alert("Phone number cannot be more than 10 digits");
-			}
-			return isValidPhoneNumber;
-		}
+			  var f = document.getElementById('file').files[0],
+				  r = new FileReader();
+			  r.onloadend = function(e)
+			  {
+				$scope.data = e.target.result;
+			  }
+			  r.readAsDataURL(f);
+		};
+
+
 		
 		function validateName()
 		{
 			var validKeyRegex = /[a-zA-Z]$/, isValidUserName = true;
-			if(!(validKeyRegex.test($scope.newUser.name)))
+			if($scope.newUser && !(validKeyRegex.test($scope.newUser.name)))
 			{
 				alert("Username cannot have numbers or Special characaters");
 				$scope.newUser.name = "";
 				isValidUserName = false;
 			}
 			return isValidUserName;
+		}
+		
+		function validateAge()
+		{
+			var isValidAge = true, validAgeRegex = /[0-9]$/;
+			if($scope.newUser && !validAgeRegex.test($scope.newUser.age))
+			{
+				isValidAge = false;
+				alert("Age can be only numbers");
+			}
+			if($scope.newUser && isValidAge && !($scope.newUser.age.length == 1 || $scope.newUser.age.length == 2))
+			{
+				isValidAge = false;
+				alert("Age cannot be more than 2 digits");
+			}
+			return isValidAge;
+		}
+		
+		function validatePhoneNumber()
+		{
+			var isValidPhoneNumber = true, validPhoneRegex = /[0-9]$/;
+			if($scope.newUser && !validPhoneRegex.test($scope.newUser.phone))
+			{
+				isValidPhoneNumber = false;
+				alert("Phone Number can be only numbers");
+			}
+			if($scope.newUser && isValidPhoneNumber && $scope.newUser.phone.length != 10)
+			{
+				isValidPhoneNumber = false;
+				alert("Phone number should be 10 digits");
+			}
+			return isValidPhoneNumber;
+		}
+		
+		function validateEmail()
+		{
+			var isValidEmail = true;
+			if($scope.newUser && $scope.newUser.email.indexOf("@")==-1)
+			{
+				isValidEmail=false;
+				alert("Please enter a valid email address");
+			}
+			
+			return isValidEmail;
 		}
 	}]);
 	//ProfileController ends
